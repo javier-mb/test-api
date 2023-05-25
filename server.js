0c673e3d0e153a47ec8bc8fd4955cbc0e7c753aa
chico del mate <chicodelmate@gmail.com>
2023-05-25 15:20:05 -0300

    first

---
+++ server.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const Product = require("./models/productModel");
+require("dotenv").config();
+
+const app = express();
+const URL_CONNECT = process.env.URL_CONNECT;
+const PORT = process.env.PORT;
+app.use(express.json());
+
+app.listen(PORT, () => console.log("Server listening in port", PORT));
+
+app.get("/", (req, res) => {
+  res.send("Hello from home");
+});
+
+app.get("/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.get("/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.delete("/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+    if (!product) {
+      return res.status(404).json({ message: `Cannot find this ID: ${id}` });
+    } else {
+      return res.status(200).json(product);
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.put("/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+    if (!product) {
+      return res.status(404).json({ message: `Cannot find this ID: ${id}` });
+    } else {
+      const productUpdate = await Product.findById(id);
+      return res.status(200).json(productUpdate);
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.post("/product", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.post;
+
+mongoose
+  .connect(URL_CONNECT)
+  .then(() => console.log("connect with mongo"))
+  .catch(() => console.log(error));
